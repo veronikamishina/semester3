7ff38755da1511e4d13e25167b8b3a229d8d973f
veronikamishina <sleepy_day@mail.ru>
2025-09-19 16:39:43 +0300

    Adding a link to task 1.3 and task 1.3 itself

--- task/app1.js
+++ task/app1.js
@@ -24,21 +24,41 @@ if (userInput === null || userInput.trim() === "") {
 }
 }
 
-// Вызов функции для выполнения
-task1();
-
 // Задание 1.2: Пользователь вводит строку. Вывести на консоль каждый воторой символ строки, начиная с последнего.
 function task2() {
     let userString = prompt("Пожалуйста, введите свою строку");
     if (userString === null) {
         console.log("Ошибка: Вы не ввели строку");
+        return task3(); // Перехожу к заданию 1.3
     }
-    
     let result = [];
     // Начинаю с последнего символа и иду назад через один
     for (let i = userString.length - 1; i >= 0; i -= 2) {
         result.push(userString[i]);
     }
     console.log("Исходная строка:", userString);
-    console.log("Каждый второй символ с конца:", result.join(''));    
+    console.log("Каждый второй символ с конца:", result.join(''));
+    
+    return task3(); // Перехожу к заданию 1.3
 }
+
+// Задание 1.3: Дан двухмерный массив с числами (задаётся в тексте программы). Найдите сумму квадратов элементов этого массива.
+function task3() {
+    const matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
+    let sumOfSquares = 0;
+    // Прохожу по всем элементам двумерного массива
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            sumOfSquares += matrix[i][j] ** 2;
+        }
+    }
+    console.log("Задан двухмерный массив:", matrix);
+    console.log("Сумма квадратов элементов массива:", sumOfSquares);
+}
+
+// Вызов функции для выполнения
+task1();
